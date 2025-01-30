24c71c46d72e14e7e93b9aaae57d89e77499170f
mailvlous <mailvelous03@gmail.com>
2025-01-30 13:57:56 +0700

    feat: add modular for malloc

--- malloc.c
+++ malloc.c
@@ -1,23 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int* array = (int*)malloc(10);
+int* createArray(int size) {
+    int* array = malloc(sizeof(int) * size);
 
     if (array == NULL) {
-        printf("Memory allocation failed\n");
-        return 1;
+        return NULL;
     }
-
-    for (int i = 0; i <0; i++) {
+    
+    for (int i = 0; i < size; i++) {
         array[i] = i;
     }
 
-    for (int i = 0; i < 10; i++) {
-        printf("%d", array[i]);
+    return array;
+}
+
+int main() {
+
+    int size = 10;
+    int count = 0;
+
+    int* array = createArray(size); 
+
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", array[i]);
+        count++;
     }
-    
+
     free(array);
+
+    // int count = 0;
+    // int size = 10;
+    // int* array = malloc(sizeof(int) * size);
+
+    // if (array == NULL) {
+    //     printf("Memory allocation failed\n");
+    //     return 1;
+    // }
+
+    // size++;
+    // int* temp = realloc(array, sizeof(int) * size);
+    // if(temp == NULL) {
+    //     printf("Memory reallocation failed\n");
+    //     return 1;
+    // } else {
+    //     array = temp;
+    // }
+
+    // for (int i = 0; i < size; i++) {
+    //     array[i] = i;
+    //     count++;
+    // }
+
+    // for (int i = 0; i < count; i++) {
+    //     printf("%d", array[i]);
+    // }
+    
+    // free(array);
+
+
 
     return 0;
 }
+
+
